918f573620a9589e6100ddbd0657b61ec00e87c9
thusharathiwanka <thusharathiwanka123@gmail.com>
2020-11-03 21:55:56 +0530

    CREATE: generate photos and search added

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
+const apiKey = "563492ad6f91700001000001c768832b78b547eb9e8a086034e76994";
+const gallery = document.querySelector(".gallery");
+const searchForm = document.querySelector(".search-form");
+const searchInput = document.querySelector(".search-input");
+const submitBtn = document.querySelector(".submit-btn");
+let searchValue;
 
+searchInput.addEventListener("input", updateSearchInput);
+
+searchForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  searchPhotos(searchValue);
+});
+
+async function fetchAPI(url) {
+  const dataFetch = await fetch(url, {
+    method: "GET",
+    headers: {
+      Accept: "application/json",
+      Authorization: apiKey,
+    },
+  });
+  const data = await dataFetch.json();
+
+  return data;
+}
+
+function updateSearchInput(event) {
+  searchValue = event.target.value;
+}
+
+async function curatedPhotos() {
+  const data = await fetchAPI("https://api.pexels.com/v1/curated?per_page=15");
+  generatePhotos(data);
+}
+
+async function searchPhotos(searchInput) {
+  const data = await fetchAPI(
+    `https://api.pexels.com/v1/search?query=${searchInput}&per_page=15`
+  );
+  generatePhotos(data);
+}
+
+function generatePhotos(data) {
+  data.photos.forEach((photo) => {
+    const galleryImg = document.createElement("div");
+    galleryImg.classList.add("gallery-img");
+    galleryImg.innerHTML = `<img src="${photo.src.large}">
+    <p>${photo.photographer}</p>`;
+    gallery.appendChild(galleryImg);
+  });
+}
+
+curatedPhotos();
